b6112e61926701d2ca89aa8e5f7bbbea26d61d05
AmalH <hichri.amal@hotmail.fr>
2018-02-19 19:48:10 +0100

    updated build.gradle

--- app/src/androidTest/java/amalhichri/androidprojects/com/authenticationSample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/amalhichri/androidprojects/com/authenticationSample/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("amalhichri.androidprojects.com.kotlinlearning", appContext.getPackageName());
+        assertEquals("amalhichri.androidprojects.com.authenticationSample", appContext.getPackageName());
     }
 }
--- app/src/main/java/amalhichri/androidprojects/com/authenticationSample/activities/MainActivity.java
+++ app/src/main/java/amalhichri/androidprojects/com/authenticationSample/activities/MainActivity.java
@@ -3,10 +3,18 @@ package amalhichri.androidprojects.com.authenticationSample.activities;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import amalhichri.androidprojects.com.authenticationSample.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -19,6 +27,9 @@ public class MainActivity extends Activity implements View.OnClickListener   {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            // called only once to generate package hashKey for Linkedin app
+        //generateHashkey();
 
 
         /** if user is logged in --> homeActivity**/
@@ -44,5 +55,24 @@ public class MainActivity extends Activity implements View.OnClickListener   {
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
+
+    /** needs to get called only once to get the package hash for Linkedin application **/
+    public void generateHashkey(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "amalhichri.androidprojects.com.authenticationSample",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d("Error", e.getMessage(), e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.d("Error", e.getMessage(), e);
+        }
+    }
+
 
 }
